cc7f6872030629a124bc2971a53697bdbf939106
Reycel Carcueva <rvcarcueva2@student.apc.edu.ph>
2024-10-22 22:26:30 +0800

    modified dashboard

--- InnolabAMS/resources/views/layouts/navigation.blade.php
+++ InnolabAMS/resources/views/layouts/navigation.blade.php
@@ -6,15 +6,15 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        <x-application-logo style="height: auto ; width: 70px;" class="fill-current text-gray-800" />
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-6 sm:flex pt-6 ">
+                    <h2 class="font-semibold text-xl text-gray-800 leading-tight "> 
+                        {{ __('Admission Management System') }}
+                    </h2>
                 </div>
             </div>
 
@@ -43,7 +43,7 @@
                             @csrf
 
                             <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
+                                onclick="event.preventDefault();
                                                 this.closest('form').submit();">
                                 {{ __('Log Out') }}
                             </x-dropdown-link>
@@ -89,7 +89,7 @@
                     @csrf
 
                     <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
+                        onclick="event.preventDefault();
                                         this.closest('form').submit();">
                         {{ __('Log Out') }}
                     </x-responsive-nav-link>
