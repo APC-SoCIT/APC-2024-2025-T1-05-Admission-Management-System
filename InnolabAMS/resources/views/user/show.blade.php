02e4013ad08b3b5d02550bd19b20f30ddaf935db
Reycel Carcueva <rvcarcueva2@student.apc.edu.ph>
2024-10-23 13:14:19 +0800

    add dashboard layout

--- InnolabAMS/resources/views/user/show.blade.php
+++ InnolabAMS/resources/views/user/show.blade.php
@@ -1,4 +1,6 @@
-<x-app-layout>
+@extends('dashboard') <!-- Use the dashboard layout -->
+
+@section('content') <!-- Define the content section -->
     <h1 class="text-2xl font-semibold mb-4 mx-4 my-4">Users</h1>
     <table class="min-w-full bg-white border border-gray-300">
         <thead>
@@ -6,7 +8,6 @@
                 <th class="py-2 px-4 border-b text-left">ID</th>
                 <th class="py-2 px-4 border-b text-left">Name</th>
                 <th class="py-2 px-4 border-b text-left">Email</th>
-                <!-- Add more headers as needed -->
             </tr>
         </thead>
         <tbody>
@@ -15,9 +16,8 @@
                     <td class="py-2 px-4 border-b">{{ $user->id }}</td>
                     <td class="py-2 px-4 border-b">{{ $user->name }}</td>
                     <td class="py-2 px-4 border-b">{{ $user->email }}</td>
-                    <!-- Add more user fields as needed -->
                 </tr>
             @endforeach
         </tbody>
     </table>
-</x-app-layout>
+@endsection
