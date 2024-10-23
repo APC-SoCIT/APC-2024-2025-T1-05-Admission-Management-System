71d943362d54b21ede237db9e17a55a5a2117799
Reycel Carcueva <rvcarcueva2@student.apc.edu.ph>
2024-10-23 12:48:48 +0800

    added user route

--- InnolabAMS/resources/views/dashboard.blade.php
+++ InnolabAMS/resources/views/dashboard.blade.php
@@ -11,7 +11,7 @@
         <div class="w-64 h-screen bg-white text-gray-800 border-r border-gray-300 flex-shrink-0">
             <ul class="space-y-6 p-6"> <!-- Increased space between items -->
                 <li>
-                    <a href="{{ route('dashboard') }}" class="flex items-center py-4 px-6 hover:bg-gray-300 rounded transition duration-200 ease-in-out">
+                    <a href="{{ route('user.show') }}" class="flex items-center py-4 px-6 hover:bg-gray-300 rounded transition duration-200 ease-in-out">
                         <i class="fa-solid fa-user mr-2"></i> <!-- Increased margin-right for the icon -->
                         <span class="ml-2">{{ __('Users') }}</span> <!-- Added margin-left to text for better spacing -->
                     </a>
@@ -22,7 +22,7 @@
 
         <!-- Content Area -->
         <div class="flex-grow p-6">
-            <!-- Content will go here -->
+        
         </div>
     </div>
 
