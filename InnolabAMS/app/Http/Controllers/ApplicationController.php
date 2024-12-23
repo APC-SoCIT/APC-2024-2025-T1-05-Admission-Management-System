608212d746c500e1782f658e80609ea38f3ed49f
Reejay Salinas <rvsalinas@student.apc.edu.ph>
2024-12-23 20:01:56 +0800

    Added file size display to document list

--- InnolabAMS/app/Http/Controllers/ApplicationController.php
+++ InnolabAMS/app/Http/Controllers/ApplicationController.php
@@ -71,6 +71,17 @@ class ApplicationController extends Controller
         ->withInput();
 }
 
+    public function formatFileSize($bytes)
+    {
+        if ($bytes >= 1048576) {
+            return number_format($bytes / 1048576, 2) . ' MB';
+        } elseif ($bytes >= 1024) {
+            return number_format($bytes / 1024, 2) . ' KB';
+        }
+        return $bytes . ' bytes';
+}
+
+
     public function viewDocument(Document $document)
     {
         // Basic auth check from middleware
