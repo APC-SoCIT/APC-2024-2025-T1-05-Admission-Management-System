a917c31b2d22f94f14f30402a7dc14d195991dd6
Althea Noelle Sarmiento <absarmiento@student.apc.edu.ph>
2024-12-27 16:32:35 +0800

    Fix basic auth check from middleware

--- InnolabAMS/routes/web.php
+++ InnolabAMS/routes/web.php
@@ -21,6 +21,11 @@ Route::middleware('guest')->group(function () {
     })->name('register');
 });
 
+// Add these profile routes
+Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
 // Auth required routes
 Route::middleware(['auth', 'verified'])->group(function () {
     // Dashboard
