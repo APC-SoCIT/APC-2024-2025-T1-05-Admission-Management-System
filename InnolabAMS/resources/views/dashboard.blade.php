1aefd5d5eb8bb5603525c439bc189f6a1c8d77bc
Reycel Carcueva <rvcarcueva2@student.apc.edu.ph>
2024-12-27 15:49:34 +0800

    modified dashboard

--- InnolabAMS/resources/views/dashboard.blade.php
+++ InnolabAMS/resources/views/dashboard.blade.php
@@ -1,13 +1,13 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Admin Panel') }}
         </h2>
     </x-slot>
 
     <div class="flex">
         <!-- Sidebar -->
-        <div class="w-64 h-screen bg-white text-gray-800 border-r border-gray-300 flex-shrink-0">
+        <div class="w-64 h-screen bg-gray-100 text-gray-800 border-r border-gray-300 flex-shrink-0">
             <ul class="space-y-6 p-6"> <!-- Increased space between items -->
                 <li>
                     <a href="{{ route('user.show') }}" class="flex items-center py-4 px-6 hover:bg-gray-300 rounded transition duration-200 ease-in-out">
