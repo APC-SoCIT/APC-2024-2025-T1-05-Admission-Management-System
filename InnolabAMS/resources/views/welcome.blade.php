e2e998b57e958fbde51bb5cd6e3ccab349698764
Reycel Carcueva <rvcarcueva2@student.apc.edu.ph>
2024-12-27 15:49:43 +0800

    modified welcome page

--- InnolabAMS/resources/views/welcome.blade.php
+++ InnolabAMS/resources/views/welcome.blade.php
@@ -8,11 +8,12 @@
     <title>InnolabAMS</title>
 
     <!-- Favicon -->
-       <link rel="icon" href="{{ asset('/static/images/innolab_logo3.png') }}" type="image/x-icon">
+       <link rel="icon" href="{{ asset('/static/images/innolab_favicon.png') }}" type="image/x-icon">
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
     <script src="//unpkg.com/alpinejs" defer></script>
     <script src="https://cdn.tailwindcss.com"></script>
@@ -962,7 +963,7 @@
                 <div class="flex gap-4 justify-center">
                     <a href="{{ route('login') }}"
                         class="rounded-md border border-gray-300 px-4 py-2 text-black transition duration-200 hover:bg-gray-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-gray-500">
-                        Log in
+                        Sign In
                     </a>
                     @if (Route::has('register'))
                     <a href="{{ route('register') }}"
