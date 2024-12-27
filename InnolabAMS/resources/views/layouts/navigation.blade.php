4ac3cab345955b413c29150f0622877fba969b62
Reycel Carcueva <rvcarcueva2@student.apc.edu.ph>
2024-12-27 15:51:19 +0800

    modified navigation

--- InnolabAMS/resources/views/layouts/navigation.blade.php
+++ InnolabAMS/resources/views/layouts/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
+<nav x-data="{ open: false }" class="bg-white dark:bg-gray-100 border-b border-gray-100 dark:border-gray-300">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
@@ -12,21 +12,11 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    @auth
-                        <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                            {{ __('Dashboard') }}
-                        </x-nav-link>
-                    @else
-                        <x-nav-link :href="route('login')" :active="request()->routeIs('login')">
-                            {{ __('Login') }}
-                        </x-nav-link>
-
-                        @if (Route::has('register'))
-                            <x-nav-link :href="route('register')" :active="request()->routeIs('register')">
-                                {{ __('Register') }}
-                            </x-nav-link>
-                        @endif
-                    @endauth
+                    <div class="hidden space-x-8 sm:-my-px sm:ms-6 sm:flex pt-6 ">
+                        <h2 class="font-semibold text-xl text-gray-800 leading-tight ">
+                            {{ __('Admission Management System') }}
+                        </h2>
+                    </div>
                 </div>
             </div>
 
@@ -34,7 +24,7 @@
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-800 bg-white hover:text-gray-700 dark:hover:text-gray-800 focus:outline-none transition ease-in-out duration-150">
                             <div>{{ Auth::user()->name }}</div>
                             <div class="ms-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
