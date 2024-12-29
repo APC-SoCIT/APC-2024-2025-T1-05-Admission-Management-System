cf0bc670af2dafb7cec14c2225be5bc7ae01d600
Althea Noelle Sarmiento <absarmiento@student.apc.edu.ph>
2024-12-29 14:53:21 +0800

    Update navigation layout to match wireframe design

--- InnolabAMS/resources/views/dashboard.blade.php
+++ InnolabAMS/resources/views/dashboard.blade.php
@@ -14,20 +14,18 @@ use Illuminate\Support\Facades\Auth;
         <div class="w-64 h-screen bg-white text-gray-800 border-r border-gray-300 flex-shrink-0">
             <ul class="space-y-6 p-6">
                 <li>
-                    <a href="{{ route('applications.index') }}" class="flex items-center py-4 px-6 hover:bg-gray-300 rounded transition duration-200 ease-in-out">
+                    <a href="{{ route('applications.index') }}"
+                       class="flex items-center py-4 px-6 hover:bg-gray-300 rounded transition duration-200 ease-in-out">
                         <i class="fa-solid fa-list mr-2"></i>
                         <span class="ml-2">{{ __('Applications') }}</span>
                     </a>
                 </li>
-                <!-- Add other sidebar items as needed -->
             </ul>
         </div>
 
         <!-- Content Area -->
         <div class="flex-grow p-6">
-            @if(Auth::check())
-                {{ __("You're logged in!") }}
-            @endif
+            {{ __("You're logged in!") }}
             @yield('content')
         </div>
     </div>
