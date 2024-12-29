03e11d4141ac03e8e177fe32c7762c6e53284d45
Reycel Carcueva <rvcarcueva2@student.apc.edu.ph>
2024-12-29 15:05:46 +0800

    modified routes

--- InnolabAMS/routes/web.php
+++ InnolabAMS/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\UserController; // Added Controller
+use App\Http\Controllers\ScholarshipController; // Added Controller
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -14,6 +15,7 @@ Route::get('/dashboard', function () {
 
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard/users', [UserController::class, 'show'])->name('user.show');
+    Route::get('/dashboard/scholarship', [ScholarshipController::class, 'show'])->name('scholarship.show');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
