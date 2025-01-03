8a7d4cbd3c6fecc5de224dd1f2447a15d1e92e17
Althea Noelle Sarmiento <absarmiento@student.apc.edu.ph>
2025-01-03 15:54:33 +0800

    modified dashboard

--- InnolabAMS/resources/views/layouts/navigation.blade.php
+++ InnolabAMS/resources/views/layouts/navigation.blade.php
@@ -73,7 +73,7 @@ use Illuminate\Support\Facades\Route;
             </a>
 
             <!-- Users -->
-            <a href="{{ route('users') }}"
+            <a href="{{ route('users.index') }}"
                class="flex items-center px-4 py-2 {{ request()->routeIs('users') ? 'bg-blue-50 text-blue-700' : 'text-gray-700 hover:bg-gray-50' }}">
                 <svg class="h-5 w-5 mr-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
