ac786b622a2d361449dcda45ccacfb53fed99f8b
JamesAlbertDavid <jbdavid@student.apc.edu.ph>
2025-01-03 20:41:25 +0800

    updated the InquiryController and added inquiry show.blade.php

--- InnolabAMS/app/Http/Controllers/InquiryController.php
+++ InnolabAMS/app/Http/Controllers/InquiryController.php
@@ -1,10 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Inquiry;
 use Illuminate\Http\Request;
 
 class InquiryController extends Controller
 {
-    //
+
+    public function show(Request $request)
+    {
+        $scholarship = Inquiry::all();
+        return view('inquiry.show', compact('inquiry'));
+    }
 }
