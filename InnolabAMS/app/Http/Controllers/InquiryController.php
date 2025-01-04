4751454f25e286bb2cd96fe9c7a69e15321fa6f1
JamesAlbertDavid <jbdavid@student.apc.edu.ph>
2025-01-04 18:13:51 +0800

    fixed the InquiryController

--- InnolabAMS/app/Http/Controllers/InquiryController.php
+++ InnolabAMS/app/Http/Controllers/InquiryController.php
@@ -1,15 +1,23 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\Inquiry;
+
 use Illuminate\Http\Request;
+use App\Models\Inquiry;
 
 class InquiryController extends Controller
 {
-
-    public function show(Request $request)
+    // Fetch all inquiries for the index page
+    public function index()
     {
-        $scholarship = Inquiry::all();
+        $inquiries = Inquiry::all(); // Fetch all inquiries
+        return view('inquiry.index', compact('inquiries'));
+    }
+
+    // Fetch a single inquiry for the show page
+    public function show($id)
+    {
+        $inquiry = Inquiry::findOrFail($id); // Fetch a single inquiry by ID
         return view('inquiry.show', compact('inquiry'));
     }
 }
