c72be3feb3d687c9f60079001d186696142c474e
Reycel Carcueva <rvcarcueva2@student.apc.edu.ph>
2025-01-05 22:35:02 +0800

    modified web.php

--- InnolabAMS/routes/web.php
+++ InnolabAMS/routes/web.php
@@ -14,7 +14,7 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/dashboard/users', [UserController::class, 'show'])->name('user.show');
+    Route::get('/dashboard/users', [UserController::class, 'show'])->name('user.show'); //Added Route
     Route::get('/dashboard/scholarship', [ScholarshipController::class, 'show'])->name('scholarship.show');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
