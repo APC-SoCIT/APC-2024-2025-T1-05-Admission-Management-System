072268461facd6bc1437e338a1a5dd74c3e5c556
Reycel Carcueva <rvcarcueva2@student.apc.edu.ph>
2025-01-07 11:10:08 +0800

    added comments

--- InnolabAMS/routes/web.php
+++ InnolabAMS/routes/web.php
@@ -17,8 +17,8 @@ Route::get('/dashboard', function () {
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard/users', [UserController::class, 'show'])->name('user.show'); //Added Route
     Route::get('/dashboard/scholarship', [ScholarshipController::class, 'show'])->name('scholarship.show'); //Added Route
-    Route::get('/dashboard/inquiry', [InquiryController::class, 'show'])->name('inquiry.show');
-    Route::get('/dashboard/inquiry', [InquiryController::class, 'index'])->name('inquiry.index');
+    Route::get('/dashboard/inquiry', [InquiryController::class, 'show'])->name('inquiry.show'); // Added Route
+    Route::get('/dashboard/inquiry', [InquiryController::class, 'index'])->name('inquiry.index'); // Added Route
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
