0993fbc9f07ce711efbce39d9c939332bbbacc86
Reycel Carcueva <rvcarcueva2@student.apc.edu.ph>
2025-01-08 14:28:37 +0800

    modified seeder

--- InnolabAMS/database/seeders/DatabaseSeeder.php
+++ InnolabAMS/database/seeders/DatabaseSeeder.php
@@ -18,6 +18,8 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => bcrypt('password'),
+            'phone_number' => '09123456789'
         ]);
     }
 }
