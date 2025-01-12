8e87f58aabb2cbf9bc78b06d742e5fabe79bbe53
Reejay Salinas <121993503+rjsalinas@users.noreply.github.com>
2025-01-12 15:18:57 +0000

    Modified family info and sibling info models

--- InnolabAMS/app/Models/FamilyInfo.php
+++ InnolabAMS/app/Models/FamilyInfo.php
@@ -8,4 +8,40 @@ use Illuminate\Database\Eloquent\Model;
 class FamilyInfo extends Model
 {
     use HasFactory;
+
+    protected $table = 'family_info';
+
+    protected $fillable = [
+        'applicant_id',
+        'father_surname',
+        'father_given_name',
+        'father_middle_name',
+        'father_occupation',
+        'father_contact_num',
+        'mother_surname',
+        'mother_given_name',
+        'mother_middle_name',
+        'mother_occupation',
+        'mother_contact_num',
+        'guardian_info',
+        'guardian_surname',
+        'guardian_given_name',
+        'guardian_middle_name',
+        'guardian_address_street',
+        'guardian_address_city',
+        'guardian_contact_num',
+        'guardian_email'
+    ];
+
+    // Relationship with ApplicantInfo
+    public function applicant()
+    {
+        return $this->belongsTo(ApplicantInfo::class, 'applicant_id');
+    }
+
+    // Relationship with siblings
+    public function siblings()
+    {
+        return $this->hasMany(SiblingInfo::class);
+    }
 }
--- InnolabAMS/app/Models/SiblingInfo.php
+++ InnolabAMS/app/Models/SiblingInfo.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class SiblingInfo extends Model
 {
     use HasFactory;
+
+    protected $table = 'siblings_info';
+
+    protected $fillable = [
+        'family_info_id',
+        'sibling_surname',
+        'sibling_given_name',
+        'sibling_age',
+        'sibling_school_name',
+        'sibling_school_address',
+        'sibling_grade_level'
+    ];
+
+    // Relationship with FamilyInfo
+    public function familyInfo()
+    {
+        return $this->belongsTo(FamilyInfo::class, 'family_info_id');
+    }
 }
