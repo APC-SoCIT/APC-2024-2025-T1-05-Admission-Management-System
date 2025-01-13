d0be965fbb4ab36a9b7957daf8a57517cf37d249
Althea Noelle Sarmiento <113029970+noe-lle@users.noreply.github.com>
2025-01-13 05:11:21 +0000

    modified educational background table

--- InnolabAMS/database/migrations/2025_01_13_050812_create_educational_background_table.php
+++ InnolabAMS/database/migrations/2025_01_13_050812_create_educational_background_table.php
@@ -6,20 +6,29 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('educational_background', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('applicant_id');
+            $table->string('lrn', 12)->nullable();
+            $table->boolean('sped')->default(false);
+            $table->boolean('pwd')->default(false);
+            $table->string('applicant_school_name')->nullable();
+            $table->string('applicant_school_address')->nullable();
+            $table->enum('applicant_last_grade_level', ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'])->nullable();
+            $table->date('applicant_year_graduation')->nullable();
+            $table->decimal('applicant_gwa', 4, 2)->nullable();
+            $table->string('applicant_achievements')->nullable();
             $table->timestamps();
+
+            $table->foreign('applicant_id')
+                  ->references('id')
+                  ->on('applicant_infos')
+                  ->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('educational_background');
