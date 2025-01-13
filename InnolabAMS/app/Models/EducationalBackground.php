ffcae44dbb301cb7983ad9e4bee710c64afe3b6a
Althea Noelle Sarmiento <113029970+noe-lle@users.noreply.github.com>
2025-01-13 05:11:57 +0000

    modified educational background model

--- InnolabAMS/app/Models/EducationalBackground.php
+++ InnolabAMS/app/Models/EducationalBackground.php
@@ -8,4 +8,34 @@ use Illuminate\Database\Eloquent\Model;
 class EducationalBackground extends Model
 {
     use HasFactory;
+
+    protected $table = 'educational_background';
+
+    protected $fillable = [
+        'applicant_id',
+        'lrn',
+        'sped',
+        'pwd',
+        'applicant_school_name',
+        'applicant_school_address',
+        'applicant_last_grade_level',
+        'applicant_year_graduation',
+        'applicant_gwa',
+        'applicant_achievements'
+    ];
+
+    protected $casts = [
+        'sped' => 'boolean',
+        'pwd' => 'boolean',
+        'applicant_year_graduation' => 'date',
+        'applicant_gwa' => 'decimal:2'
+    ];
+
+    /**
+     * Get the applicant that owns the educational background.
+     */
+    public function applicant()
+    {
+        return $this->belongsTo(ApplicantInfo::class, 'applicant_id');
+    }
 }
