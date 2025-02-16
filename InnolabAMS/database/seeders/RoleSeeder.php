a18efa2b8efe03135ae3394763bc663d8a750c38
Reycel John Emmanuel Carcueva <rvcarcueva2@student.apc.edu.ph>
2025-02-16 15:14:55 +0000

    added applicant role

--- InnolabAMS/database/seeders/RoleSeeder.php
+++ InnolabAMS/database/seeders/RoleSeeder.php
@@ -14,15 +14,27 @@ class RoleSeeder extends Seeder
         // Check and create roles if they do not exist
         $adminRole = Role::firstOrCreate(['name' => 'Admin']);
         $staffRole = Role::firstOrCreate(['name' => 'Staff']);
+        $applicantRole = Role::firstOrCreate(['name' => 'Applicant']);
 
         // Check and create permissions if they do not exist
         Permission::firstOrCreate(['name' => 'view admissions']);
         Permission::firstOrCreate(['name' => 'view scholarships']);
         Permission::firstOrCreate(['name' => 'view inquiries']);
+        Permission::firstOrCreate(['name' => 'view personal information']);
+        Permission::firstOrCreate(['name' => 'view educational background']);
+        Permission::firstOrCreate(['name' => 'view family information']);
+        Permission::firstOrCreate(['name' => 'view additional information']);
 
         // Assign permissions to roles
         $adminRole->givePermissionTo(['view admissions', 'view scholarships', 'view inquiries']);
         $staffRole->givePermissionTo(['view admissions', 'view scholarships', 'view inquiries']);
+        $applicantRole->givePermissionTo([
+            'view personal information',
+            'view educational background',
+            'view family information',
+            'view additional information',
+            'view scholarships'
+        ]);
 
         // Assign roles to users
         $admin = User::find(1); // Assuming the first user is the admin
@@ -33,6 +45,11 @@ class RoleSeeder extends Seeder
         $staff = User::find(2); // Assuming the second user is a staff member
         if ($staff && !$staff->hasRole('Staff')) {
             $staff->assignRole('Staff');
+        }
+
+        $applicant = User::find(3); // Assuming the third user is an applicant
+        if ($applicant && !$applicant->hasRole('Applicant')) {
+            $applicant->assignRole('Applicant');
         }
     }
 }
