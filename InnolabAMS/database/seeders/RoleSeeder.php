3345da8bfd5844aa21d2b25537410417709b6e4c
Reycel John Emmanuel Carcueva <rvcarcueva2@student.apc.edu.ph>
2025-02-17 04:11:19 +0000

    fix account order

--- InnolabAMS/database/seeders/RoleSeeder.php
+++ InnolabAMS/database/seeders/RoleSeeder.php
@@ -12,19 +12,19 @@ class RoleSeeder extends Seeder
 {
     public function run()
     {
-        // Check and create roles if they do not exist
-        $adminRole = Role::firstOrCreate(['name' => 'Admin']);
-        $staffRole = Role::firstOrCreate(['name' => 'Staff']);
-        $applicantRole = Role::firstOrCreate(['name' => 'Applicant']);
+        // Create roles
+        $adminRole = Role::create(['name' => 'Admin']);
+        $staffRole = Role::create(['name' => 'Staff']);
+        $applicantRole = Role::create(['name' => 'Applicant']);
 
-        // Check and create permissions if they do not exist
-        Permission::firstOrCreate(['name' => 'view admissions']);
-        Permission::firstOrCreate(['name' => 'view scholarships']);
-        Permission::firstOrCreate(['name' => 'view inquiries']);
-        Permission::firstOrCreate(['name' => 'view personal information']);
-        Permission::firstOrCreate(['name' => 'view educational background']);
-        Permission::firstOrCreate(['name' => 'view family information']);
-        Permission::firstOrCreate(['name' => 'view additional information']);
+        // Create permissions
+        Permission::create(['name' => 'view admissions']);
+        Permission::create(['name' => 'view scholarships']);
+        Permission::create(['name' => 'view inquiries']);
+        Permission::create(['name' => 'view personal information']);
+        Permission::create(['name' => 'view educational background']);
+        Permission::create(['name' => 'view family information']);
+        Permission::create(['name' => 'view additional information']);
 
         // Assign permissions to roles
         $adminRole->givePermissionTo(['view admissions', 'view scholarships', 'view inquiries']);
@@ -37,37 +37,28 @@ class RoleSeeder extends Seeder
             'view scholarships'
         ]);
 
-        // Create default admin user if it does not exist
-        $admin = User::firstOrCreate(
-            ['email' => 'admin@example.com'],
-            [
-                'name' => 'Admin',
-                'password' => Hash::make('password') // Change this to a secure password
-            ]
-        );
+        // Create admin user and assign role
+        $admin = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password') // Change this to a secure password
+        ]);
+        $admin->assignRole($adminRole);
 
-        // Assign roles to users
-        if ($admin && !$admin->hasRole('Admin')) {
-            $admin->assignRole('Admin');
-        }
+        // Create staff user and assign role
+        $staff = User::create([
+            'name' => 'Staff',
+            'email' => 'staff@example.com',
+            'password' => Hash::make('password') // Change this to a secure password
+        ]);
+        $staff->assignRole($staffRole);
 
-        // Create default applicant user if it does not exist
-        $applicant = User::firstOrCreate(
-            ['email' => 'applicant@example.com'],
-            [
-                'name' => 'Applicant',
-                'password' => Hash::make('password') // Change this to a secure password
-            ]
-        );
-
-        // Assign roles to users
-        if ($applicant && !$applicant->hasRole('Applicant')) {
-            $applicant->assignRole('Applicant');
-        }
-
-        $staff = User::find(2); // Assuming the second user is a staff member
-        if ($staff && !$staff->hasRole('Staff')) {
-            $staff->assignRole('Staff');
-        }
+        // Create applicant user and assign role
+        $applicant = User::create([
+            'name' => 'Applicant',
+            'email' => 'applicant@example.com',
+            'password' => Hash::make('password') // Change this to a secure password
+        ]);
+        $applicant->assignRole($applicantRole);
     }
 }
