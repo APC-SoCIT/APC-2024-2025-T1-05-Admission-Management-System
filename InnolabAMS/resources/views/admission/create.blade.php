8b624a38306abcec68a376a390d232408dbaa976
Reejay Salinas <rvsalinas@student.apc.edu.ph>
2025-02-17 09:49:29 +0800

    <<test3>>

--- InnolabAMS/resources/views/admission/create.blade.php
+++ InnolabAMS/resources/views/admission/create.blade.php
@@ -16,7 +16,7 @@
             
             <!-- Program Information -->
             <div class="mb-8">
-                <h2 class="text-xl font-semibold mb-4 pb-2 border-b">Program Information</h2>
+                <h2 class="text-xl font-semibold mb-4 pb-2 border-b">Program Informationn</h2>
                 <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
                     <div>
                         <label class="block text-sm font-medium text-gray-700">Program</label>
