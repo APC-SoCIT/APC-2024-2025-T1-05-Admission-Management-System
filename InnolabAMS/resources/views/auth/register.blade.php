356f3f82a4b4268474f98386670a10448b1dee48
Reycel John Emmanuel Carcueva <rvcarcueva2@student.apc.edu.ph>
2025-02-22 04:51:31 +0000

    added required indicator

--- InnolabAMS/resources/views/auth/register.blade.php
+++ InnolabAMS/resources/views/auth/register.blade.php
@@ -5,7 +5,9 @@
 
         <!-- Name -->
         <div>
-            <x-input-label for="name" :value="__('Name')" class="!text-black" />
+            <x-input-label for="name" class="!text-black">
+                {{ __('Name') }} <span class="text-red-500">*</span>
+            </x-input-label>
             <x-text-input id="name" class="block mt-1 w-full !bg-white !text-black border border-gray-300 rounded"
                 type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
@@ -13,7 +15,9 @@
 
         <!-- Email Address -->
         <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" class="!text-black" />
+            <x-input-label for="email" class="!text-black">
+                {{ __('Email') }} <span class="text-red-500">*</span>
+            </x-input-label>
             <x-text-input id="email" class="block mt-1 w-full !bg-white !text-black border border-gray-300 rounded"
                 type="email" name="email" :value="old('email')" required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
@@ -21,7 +25,9 @@
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" class="!text-black" />
+            <x-input-label for="password" class="!text-black">
+                {{ __('Password') }} <span class="text-red-500">*</span>
+            </x-input-label>
             <x-text-input id="password" class="block mt-1 w-full !bg-white !text-black border border-gray-300 rounded"
                 type="password" name="password" required autocomplete="new-password" />
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
@@ -29,7 +35,9 @@
 
         <!-- Confirm Password -->
         <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" class="!text-black" />
+            <x-input-label for="password_confirmation" class="!text-black">
+                {{ __('Confirm Password') }} <span class="text-red-500">*</span>
+            </x-input-label>
             <x-text-input id="password_confirmation"
                 class="block mt-1 w-full !bg-white !text-black border border-gray-300 rounded" type="password"
                 name="password_confirmation" required autocomplete="new-password" />
@@ -49,16 +57,16 @@
                 </span>
             </label>
         </div>
-        
-        <div class="flex items-center justify-between mt-4">
-        <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            href="{{ route('login') }}">
-            {{ __('Already registered?') }}
-        </a>
 
-        <x-primary-button class="ms-4">
-            {{ __('Register') }}
-        </x-primary-button>
+        <div class="flex items-center justify-between mt-4">
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                href="{{ route('login') }}">
+                {{ __('Already registered?') }}
+            </a>
+
+            <x-primary-button class="ms-4">
+                {{ __('Register') }}
+            </x-primary-button>
         </div>
     </form>
 </x-guest-layout>
