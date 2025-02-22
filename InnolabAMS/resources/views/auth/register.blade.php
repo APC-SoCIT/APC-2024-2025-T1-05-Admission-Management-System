9a2bd8914ff5c0363456d52ac72c112c4ea411e5
Reycel John Emmanuel Carcueva <rvcarcueva2@student.apc.edu.ph>
2025-02-22 02:05:53 +0000

    fixed ui forms

--- InnolabAMS/resources/views/auth/register.blade.php
+++ InnolabAMS/resources/views/auth/register.blade.php
@@ -6,39 +6,59 @@
         <!-- Name -->
         <div>
             <x-input-label for="name" :value="__('Name')" class="!text-black" />
-            <x-text-input id="name" class="block mt-1 w-full !bg-white !text-black border border-gray-300 rounded" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            <x-text-input id="name" class="block mt-1 w-full !bg-white !text-black border border-gray-300 rounded"
+                type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
         <!-- Email Address -->
         <div class="mt-4">
             <x-input-label for="email" :value="__('Email')" class="!text-black" />
-            <x-text-input id="email" class="block mt-1 w-full !bg-white !text-black border border-gray-300 rounded" type="email" name="email" :value="old('email')" required autocomplete="username" />
+            <x-text-input id="email" class="block mt-1 w-full !bg-white !text-black border border-gray-300 rounded"
+                type="email" name="email" :value="old('email')" required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" class="!text-black" />
-            <x-text-input id="password" class="block mt-1 w-full !bg-white !text-black border border-gray-300 rounded" type="password" name="password" required autocomplete="new-password" />
+            <x-text-input id="password" class="block mt-1 w-full !bg-white !text-black border border-gray-300 rounded"
+                type="password" name="password" required autocomplete="new-password" />
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <!-- Confirm Password -->
         <div class="mt-4">
             <x-input-label for="password_confirmation" :value="__('Confirm Password')" class="!text-black" />
-            <x-text-input id="password_confirmation" class="block mt-1 w-full !bg-white !text-black border border-gray-300 rounded" type="password" name="password_confirmation" required autocomplete="new-password" />
+            <x-text-input id="password_confirmation"
+                class="block mt-1 w-full !bg-white !text-black border border-gray-300 rounded" type="password"
+                name="password_confirmation" required autocomplete="new-password" />
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
+        <!-- Data Privacy Agreement -->
+        <div class="mt-6 form-group">
+            <label for="data_privacy" class="inline-flex items-center">
+                <input id="data_privacy" type="checkbox" required
+                    class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500">
+                <span class="ms-2 text-m text-gray">
+                    I agree to the
+                    <a href="#" onclick="openPrivacyPolicy()" class="text-blue-500 underline">
+                        Data Privacy Policy
+                    </a>.
+                </span>
+            </label>
+        </div>
+        
+        <div class="flex items-center justify-between mt-4">
+        <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            href="{{ route('login') }}">
+            {{ __('Already registered?') }}
+        </a>
 
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
+        <x-primary-button class="ms-4">
+            {{ __('Register') }}
+        </x-primary-button>
         </div>
     </form>
 </x-guest-layout>
--- InnolabAMS/resources/views/layouts/guest.blade.php
+++ InnolabAMS/resources/views/layouts/guest.blade.php
@@ -19,6 +19,52 @@
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
+<style>
+    h1 {
+        text-align: center;
+        color: #333;
+    }
+
+    .checkbox-group {
+        display: flex;
+        align-items: center;
+        gap: 10px;
+        margin-bottom: 10px;
+    }
+
+    /* Data Privacy Modal */
+    .modal {
+        display: none;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: rgba(0, 0, 0, 0.5);
+        justify-content: center;
+        align-items: center;
+    }
+
+    .modal-content {
+        background: white;
+        padding: 20px;
+        border-radius: 10px;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+        width: 80%;
+        max-width: 900px;
+        height: auto;
+        max-height: 90vh;
+        overflow-y: auto;
+        padding: 20px;
+    }
+
+    .modal-content div {
+        text-align: left;
+        line-height: 1.8;
+        padding: 25px;
+    }
+</style>
+
 
 <body class="font-sans antialiased bg-gray-100 text-black"
     x-data="{ showAuthLinks: false, activeButton: '', buttonsVisible: true }">
@@ -30,7 +76,7 @@
 
             <!-- Text Next to Logo -->
             <div>
-                <h1 class="text-2xl font-bold mb-1">InnolabAMS</h1>
+                <h2 class="text-2xl font-bold mb-1">InnolabAMS</h2>
                 <h3 class="text-lg">Your innovation solution partner.</h3>
             </div>
         </div>
@@ -52,11 +98,38 @@
             </a>
         </div>
     </div>
+
+    <!-- Data Privacy Modal -->
+    <div id="privacyModal" class="modal">
+        <div class="modal-content">
+            <div class="prose max-w-none">
+                {!! Str::markdown(file_get_contents(resource_path('markdown/policy.md'))) !!}
+            </div>
+            <div class="flex justify-end mt-4">
+                <x-danger-button onclick="closePrivacyPolicy()">
+                    Close
+                </x-danger-button>
+            </div>
+        </div>
+        </>
+    </div>
+
+
 </body>
 
+<script>
+    function openPrivacyPolicy() {
+        document.getElementById('privacyModal').style.display = 'flex';
+    }
+
+    function closePrivacyPolicy() {
+        document.getElementById('privacyModal').style.display = 'none';
+    }
+</script>
+
 <!-- Footer -->
-<footer class="w-full text-center text-sm py-4  text-gray-500 mt-4">
-    <p>Copyright © 2025. All Rights Reserved. Developed by Innolab</p>
+<footer class="w-full text-center text-sm py-10  text-gray-500 mt-4">
+    <p>Copyright © 2025. All Rights Reserved. Developed by Team Innolab</p>
 </footer>
 
 </html>
