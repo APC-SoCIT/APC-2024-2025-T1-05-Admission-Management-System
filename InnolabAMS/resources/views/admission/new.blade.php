6ed5ddd763a805fed1697b07e0f0a7bfecfe61fa
Reejay Salinas <rvsalinas@student.apc.edu.ph>
2025-02-23 10:21:17 +0800

    style: Center align "New" status text in applicant list

--- InnolabAMS/resources/views/admission/new.blade.php
+++ InnolabAMS/resources/views/admission/new.blade.php
@@ -91,7 +91,7 @@
                         <td class="px-6 py-4 text-center whitespace-nowrap">{{ $applicant->applicant_email }}</td>
                         <td class="px-6 py-4 text-center whitespace-nowrap">{{ $applicant->applicant_mobile_number }}</td>
                         <td class="px-6 py-4 whitespace-nowrap">
-                            <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-yellow-100 text-yellow-800">
+                            <span class="px-2 text-center inline-flex text-xs leading-5 font-semibold rounded-full bg-yellow-100 text-yellow-800">
                                 New
                             </span>
                         </td>
