1eac836842558f73cdc6a727d2a89b1bf1a812e0
Althea Noelle Sarmiento <absarmiento@student.apc.edu.ph>
2025-03-04 09:37:54 +0800

    feat: Add example text to school name input field

--- InnolabAMS/resources/views/personal_information/create.blade.php
+++ InnolabAMS/resources/views/personal_information/create.blade.php
@@ -261,7 +261,7 @@
                         </div>
                     </div>
                     <div>
-                        <label class="block text-sm font-medium text-gray-700">School Name <span class="text-red-500">*</span></label>
+                        <label class="block text-sm font-medium text-gray-700">School Name <span class="text-red-500">*</span></label> <span class="block text-sm font-medium text-gray-700">Example: Manila National High School</span>
                         <div class="flex items-center">
                             <input type="text" name="school_name" required class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
                         </div>
