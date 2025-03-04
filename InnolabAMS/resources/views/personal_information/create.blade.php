5487f1696bd3f7dac3ef70182b1d9c42d0aa327e
Althea Noelle Sarmiento <absarmiento@student.apc.edu.ph>
2025-03-04 09:57:12 +0800

    feat: Add tooltip to emergency contact's email input field

--- InnolabAMS/resources/views/personal_information/create.blade.php
+++ InnolabAMS/resources/views/personal_information/create.blade.php
@@ -552,6 +552,7 @@
                         <label class="block text-sm font-medium text-gray-700">Email <span class="text-red-500">*</span></label> <span class="block text-sm font-medium text-gray-700">Example: juan@example.com</span>
                         <div class="flex items-center">
                             <input type="email" name="emergency_contact_email" required class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                            <x-form-tooltip text="Enter the email address of the emergency contact" />
                         </div>
                     </div>
                 </div>
