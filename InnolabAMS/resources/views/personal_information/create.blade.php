21dd62c2dc2c2f33e5c69b0c560de89ebaea2673
Althea Noelle Sarmiento <absarmiento@student.apc.edu.ph>
2025-03-04 09:56:11 +0800

    feat: Add tooltip to emergency contact's complete name input field

--- InnolabAMS/resources/views/personal_information/create.blade.php
+++ InnolabAMS/resources/views/personal_information/create.blade.php
@@ -514,6 +514,7 @@
                         <label class="block text-sm font-medium text-gray-700">Complete Name <span class="text-red-500">*</span></label> <span class="block text-sm font-medium text-gray-700">Example: Juan Dela Cruz</span>
                         <div class="flex items-center">
                             <input type="text" name="emergency_contact_name" required class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                            <x-form-tooltip text="Enter the complete name of the emergency contact" />
                         </div>
                     </div>
                     <div class="md:col-span-2">
