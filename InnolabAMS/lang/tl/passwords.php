96b5d5093da34f80d33d83d7386c7977389b574c
Althea Noelle Sarmiento <absarmiento@student.apc.edu.ph>
2025-03-04 13:44:10 +0800

    Added passwords translation for Tagalog

---
+++ InnolabAMS/lang/tl/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'reset' => 'Na-reset na ang iyong password.',
+    'sent' => 'Na-email na namin ang iyong password reset link.',
+    'throttled' => 'Maghintay bago subukan muli.',
+    'token' => 'Hindi valid ang password reset token na ito.',
+    'user' => 'Hindi namin mahanap ang user na may ganitong email address.',
+];
