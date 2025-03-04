a1ea4b50f63e5556b3947f6c52653fede029c844
Althea Noelle Sarmiento <absarmiento@student.apc.edu.ph>
2025-03-04 09:23:40 +0800

    refactor: Remove placeholder text from given name input field

--- InnolabAMS/resources/views/personal_information/create.blade.php
+++ InnolabAMS/resources/views/personal_information/create.blade.php
@@ -92,7 +92,6 @@
                             <input type="text" name="applicant_given_name" required class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
                             <x-form-tooltip text="Enter your first name exactly as it appears on official documents" />
                         </div>
-                        <span class="block text-sm font-medium text-gray-700">Example: Juan</span>
                     </div>
                     <div>
                         <label class="block text-sm font-medium text-gray-700">Middle Name</label>
