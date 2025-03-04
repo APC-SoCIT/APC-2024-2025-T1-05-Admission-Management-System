f9f5d46899ab3e64bc7df82b5451e9a8228aad1b
Althea Noelle Sarmiento <absarmiento@student.apc.edu.ph>
2025-03-04 09:43:14 +0800

    feat: Add tooltip to awards/honors input field

--- InnolabAMS/resources/views/personal_information/create.blade.php
+++ InnolabAMS/resources/views/personal_information/create.blade.php
@@ -295,6 +295,7 @@
                         <label class="block text-sm font-medium text-gray-700">Awards/Honors</label> <span class="block text-sm font-medium text-gray-700">Example: Best in Mathematics, Dean's List, etc.</span>
                         <div class="flex items-center">
                             <input type="text" name="awards_honors" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                            <x-form-tooltip text="Enter any awards or honors you received" />
                         </div>
                     </div>
                     <div>
