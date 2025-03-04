3cc3c2d669b83128c5c5fdf8ea51f49b1e69e350
Althea Noelle Sarmiento <absarmiento@student.apc.edu.ph>
2025-03-04 09:07:22 +0800

    style: enhance form tooltips visibility and contrast
    - Changed tooltip background to blue for better visibility
    - Updated info icon color to match theme
    - Added shadow effect for depth perception
    - Improved tooltip arrow styling
    - Fixed tooltip text contrast issues

--- InnolabAMS/resources/views/components/form-tooltip.blade.php
+++ InnolabAMS/resources/views/components/form-tooltip.blade.php
@@ -6,7 +6,7 @@
          @mouseleave="isVisible = false"
          class="cursor-help">
         <svg xmlns="http://www.w3.org/2000/svg"
-             class="h-5 w-5 text-gray-400 hover:text-gray-500"
+             class="h-5 w-5 text-blue-500 hover:text-blue-600"
              fill="none"
              viewBox="0 0 24 24"
              stroke="currentColor">
@@ -16,15 +16,20 @@
                   d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
         </svg>
         <div x-show="isVisible"
+             x-cloak
              x-transition:enter="transition ease-out duration-200"
              x-transition:enter-start="opacity-0 translate-y-1"
              x-transition:enter-end="opacity-100 translate-y-0"
              x-transition:leave="transition ease-in duration-150"
              x-transition:leave-start="opacity-100 translate-y-0"
              x-transition:leave-end="opacity-0 translate-y-1"
-             class="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-48 px-4 py-2 bg-gray-900 rounded-lg text-sm text-white z-10">
+             class="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-48 px-4 py-2 bg-blue-600 text-white rounded-lg text-sm shadow-lg z-50">
             {{ $text }}
-            <div class="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2 rotate-45 w-2 h-2 bg-gray-900"></div>
+            <div class="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2 rotate-45 w-2 h-2 bg-blue-600"></div>
         </div>
     </div>
 </div>
+
+<style>
+    [x-cloak] { display: none !important; }
+</style>
