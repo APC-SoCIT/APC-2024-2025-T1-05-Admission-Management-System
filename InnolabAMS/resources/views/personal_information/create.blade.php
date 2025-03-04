67d90551f2ee1a2bb069507f18684b6af6d308d6
Althea Noelle Sarmiento <absarmiento@student.apc.edu.ph>
2025-03-04 09:52:02 +0800

    feat: Add tooltip to mother's last name input field

--- InnolabAMS/resources/views/personal_information/create.blade.php
+++ InnolabAMS/resources/views/personal_information/create.blade.php
@@ -373,6 +373,7 @@
                             <label class="block text-sm font-medium text-gray-700">Last Name</label> <span class="block text-sm font-medium text-gray-700">Example: Dela Cruz</span>
                             <div class="flex items-center">
                                 <input type="text" name="mother_surname" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                                <x-form-tooltip text="Enter your mother's last name" />
                             </div>
                         </div>
                         <div class="md:col-span-2">
