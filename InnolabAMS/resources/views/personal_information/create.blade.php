ac5c378211b5d971b11a5259bb8cd6f4029c82f0
Althea Noelle Sarmiento <absarmiento@student.apc.edu.ph>
2025-03-04 09:24:00 +0800

    feat: Add example text to given name input field

--- InnolabAMS/resources/views/personal_information/create.blade.php
+++ InnolabAMS/resources/views/personal_information/create.blade.php
@@ -87,7 +87,7 @@
                         </div>
                     </div>
                     <div>
-                        <label class="block text-sm font-medium text-gray-700">Given Name <span class="text-red-500">*</span></label>
+                        <label class="block text-sm font-medium text-gray-700">Given Name <span class="text-red-500">*</span></label> <span class="block text-sm font-medium text-gray-700">Example: Juan</span>
                         <div class="flex items-center">
                             <input type="text" name="applicant_given_name" required class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
                             <x-form-tooltip text="Enter your first name exactly as it appears on official documents" />
