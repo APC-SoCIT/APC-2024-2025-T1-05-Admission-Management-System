a9a0fcfc720b391ba10663fdc9ed5ba46834a42f
Althea Noelle Sarmiento <absarmiento@student.apc.edu.ph>
2025-03-04 09:43:29 +0800

    feat: Add example text to GWA input field

--- InnolabAMS/resources/views/personal_information/create.blade.php
+++ InnolabAMS/resources/views/personal_information/create.blade.php
@@ -299,7 +299,7 @@
                         </div>
                     </div>
                     <div>
-                        <label class="block text-sm font-medium text-gray-700">General Weighted Average (GWA)</label>
+                        <label class="block text-sm font-medium text-gray-700">General Weighted Average (GWA)</label> <span class="block text-sm font-medium text-gray-700">Example: 1.8</span>
                         <div class="flex items-center">
                             <input type="text" name="gwa" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
                         </div>
