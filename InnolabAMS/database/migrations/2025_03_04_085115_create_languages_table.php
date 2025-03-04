669f2719e4048f443ffad6d0121eec14d3fd0c5a
Althea Noelle Sarmiento <absarmiento@student.apc.edu.ph>
2025-03-04 16:57:20 +0800

    Update languages table migration to support 3-letter language codes

--- InnolabAMS/database/migrations/2025_03_04_085115_create_languages_table.php
+++ InnolabAMS/database/migrations/2025_03_04_085115_create_languages_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('languages', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('code', 2); // en, fil, etc
+            $table->string('code', 3); // Changed from 2 to 3 to accommodate 'fil'
             $table->boolean('is_default')->default(false);
             $table->timestamps();
         });
