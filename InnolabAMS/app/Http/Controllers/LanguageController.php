158a37a413dcfbd06daf2c2cc4f6bf9086f3898e
Althea Noelle Sarmiento <absarmiento@student.apc.edu.ph>
2025-03-04 16:32:46 +0800

    refactor: enhance language switch logging and error response

--- InnolabAMS/app/Http/Controllers/LanguageController.php
+++ InnolabAMS/app/Http/Controllers/LanguageController.php
@@ -5,33 +5,40 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Log;
 
 class LanguageController extends Controller
 {
     public function switchLang(Request $request)
     {
         try {
+            Log::info('Language switch request received', [
+                'request_data' => $request->all(),
+                'headers' => $request->headers->all()
+            ]);
+
             $lang = $request->input('lang');
-            \Log::info('Language switch attempted', ['requested_lang' => $lang]);
 
             if (!in_array($lang, ['en', 'tl'])) {
+                Log::warning('Invalid language selected', ['lang' => $lang]);
                 throw new \Exception('Invalid language selected');
             }
 
             Session::put('locale', $lang);
             App::setLocale($lang);
 
-            \Log::info('Language switched successfully', [
-                'locale' => App::getLocale(),
+            Log::info('Language switched successfully', [
+                'new_locale' => App::getLocale(),
                 'session_locale' => Session::get('locale')
             ]);
 
             return response()->json([
                 'status' => 'success',
-                'message' => 'Language switched successfully'
+                'message' => 'Language switched successfully',
+                'locale' => App::getLocale()
             ]);
         } catch (\Exception $e) {
-            \Log::error('Language switch failed', [
+            Log::error('Language switch failed', [
                 'error' => $e->getMessage(),
                 'trace' => $e->getTraceAsString()
             ]);
