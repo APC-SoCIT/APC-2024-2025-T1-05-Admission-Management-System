843a31bea964ed1a79ef6dea1a5e2d9a07b178e8
Althea Noelle Sarmiento <absarmiento@student.apc.edu.ph>
2025-03-04 09:56:58 +0800

    feat: Add example text to emergency contact's email input field

--- InnolabAMS/resources/views/personal_information/create.blade.php
+++ InnolabAMS/resources/views/personal_information/create.blade.php
@@ -549,7 +549,7 @@
                         </div>
                     </div>
                     <div class="md:col-span-2">
-                        <label class="block text-sm font-medium text-gray-700">Email <span class="text-red-500">*</span></label>
+                        <label class="block text-sm font-medium text-gray-700">Email <span class="text-red-500">*</span></label> <span class="block text-sm font-medium text-gray-700">Example: juan@example.com</span>
                         <div class="flex items-center">
                             <input type="email" name="emergency_contact_email" required class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
                         </div>
