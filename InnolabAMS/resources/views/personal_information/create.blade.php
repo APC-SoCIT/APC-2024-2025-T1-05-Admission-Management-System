784725d417228f596fc26521a8362fd60fc24cac
Althea Noelle Sarmiento <absarmiento@student.apc.edu.ph>
2025-03-04 09:43:44 +0800

    feat: Add tooltip to GWA input field

--- InnolabAMS/resources/views/personal_information/create.blade.php
+++ InnolabAMS/resources/views/personal_information/create.blade.php
@@ -302,6 +302,7 @@
                         <label class="block text-sm font-medium text-gray-700">General Weighted Average (GWA)</label> <span class="block text-sm font-medium text-gray-700">Example: 1.8</span>
                         <div class="flex items-center">
                             <input type="text" name="gwa" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                            <x-form-tooltip text="Enter your General Weighted Average (GWA) if applicable" />
                         </div>
                     </div>
                 </div>
