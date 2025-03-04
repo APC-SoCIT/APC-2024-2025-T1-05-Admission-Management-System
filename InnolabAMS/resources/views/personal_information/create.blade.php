88a8aa5db8fcbcfecf46eefd12887cd7d003644c
Althea Noelle Sarmiento <absarmiento@student.apc.edu.ph>
2025-03-04 09:26:06 +0800

    feat: Add example text to extension name input field

--- InnolabAMS/resources/views/personal_information/create.blade.php
+++ InnolabAMS/resources/views/personal_information/create.blade.php
@@ -101,7 +101,7 @@
                         </div>
                     </div>
                     <div>
-                        <label class="block text-sm font-medium text-gray-700">Extension Name</label>
+                        <label class="block text-sm font-medium text-gray-700">Extension Name</label> <span class="block text-sm font-medium text-gray-700">Example: Jr., II, III, etc.</span>
                         <div class="flex items-center">
                             <input type="text" name="applicant_extension" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
                             <x-form-tooltip text="Add name extensions like Jr., Sr., III if applicable" />
