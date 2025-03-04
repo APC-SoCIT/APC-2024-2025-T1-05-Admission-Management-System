2a8a52d6cd40c16325fbd76421789c9cf4067d0d
Althea Noelle Sarmiento <absarmiento@student.apc.edu.ph>
2025-03-04 09:38:10 +0800

    feat: Add tooltip to school name input field

--- InnolabAMS/resources/views/personal_information/create.blade.php
+++ InnolabAMS/resources/views/personal_information/create.blade.php
@@ -264,6 +264,7 @@
                         <label class="block text-sm font-medium text-gray-700">School Name <span class="text-red-500">*</span></label> <span class="block text-sm font-medium text-gray-700">Example: Manila National High School</span>
                         <div class="flex items-center">
                             <input type="text" name="school_name" required class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                            <x-form-tooltip text="Enter the name of the school you attended" />
                         </div>
                     </div>
                     <div class="md:col-span-2">
