e5fed00db4b601c4ed2dc1b85c8eff409b62a9d3
Althea Noelle Sarmiento <absarmiento@student.apc.edu.ph>
2025-03-04 09:54:33 +0800

    feat: Add tooltip to guardian's middle name input field

--- InnolabAMS/resources/views/personal_information/create.blade.php
+++ InnolabAMS/resources/views/personal_information/create.blade.php
@@ -404,6 +404,7 @@
                             <label class="block text-sm font-medium text-gray-700">Middle Name</label> <span class="block text-sm font-medium text-gray-700">Example: Santos</span>
                             <div class="flex items-center">
                                 <input type="text" name="guardian_middle_name" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                                <x-form-tooltip text="Enter your guardian's middle name" />
                             </div>
                         </div>
                         <div>
