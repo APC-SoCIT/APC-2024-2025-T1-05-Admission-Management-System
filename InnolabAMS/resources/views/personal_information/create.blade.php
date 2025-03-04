b2d8ca60d94fd85b610c7b65a36be9a868baf505
Althea Noelle Sarmiento <absarmiento@student.apc.edu.ph>
2025-03-04 09:25:48 +0800

    refactor: Remove placeholder text from Extension Name input field

--- InnolabAMS/resources/views/personal_information/create.blade.php
+++ InnolabAMS/resources/views/personal_information/create.blade.php
@@ -106,7 +106,6 @@
                             <input type="text" name="applicant_extension" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
                             <x-form-tooltip text="Add name extensions like Jr., Sr., III if applicable" />
                         </div>
-                        <span class="block text-sm font-medium text-gray-700">Example: Jr., II, III, etc.</span>
                     </div>
                     <div>
                         <label class="block text-sm font-medium text-gray-700">Sex <span class="text-red-500">*</span></label>
