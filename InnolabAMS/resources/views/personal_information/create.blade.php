265bbd44ff92ff331354cb3979e2f45fa1930715
Althea Noelle Sarmiento <absarmiento@student.apc.edu.ph>
2025-03-04 09:49:13 +0800

    feat: Add tooltip to mother's middle name input field

--- InnolabAMS/resources/views/personal_information/create.blade.php
+++ InnolabAMS/resources/views/personal_information/create.blade.php
@@ -366,6 +366,7 @@
                             <label class="block text-sm font-medium text-gray-700">Middle Name</label> <span class="block text-sm font-medium text-gray-700">Example: Santos</span>
                             <div class="flex items-center">
                                 <input type="text" name="mother_middle_name" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                                <x-form-tooltip text="Enter your mother's middle name" />
                             </div>
                         </div>
                         <div>
