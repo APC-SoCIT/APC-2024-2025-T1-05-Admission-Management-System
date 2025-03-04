b9dc98bf0093220ac032e94f28621ec893d2fa18
Althea Noelle Sarmiento <absarmiento@student.apc.edu.ph>
2025-03-04 13:42:31 +0800

    Added auth.php for Tagalog

---
+++ InnolabAMS/lang/tl/auth.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'failed' => 'Ang iyong credentials ay hindi tumutugma sa aming records.',
+    'password' => 'Mali ang password na ibinigay.',
+    'throttle' => 'Masyadong maraming pagtatangka sa pag-login. Pakisubukan muli sa loob ng :seconds segundo.',
+];
