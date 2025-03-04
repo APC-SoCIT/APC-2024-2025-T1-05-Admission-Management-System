1be1cf1ef117f2ff44eee59f6a4eac979fed7f64
Althea Noelle Sarmiento <absarmiento@student.apc.edu.ph>
2025-03-04 09:33:17 +0800

    feat: Add example text to nationality input field

--- InnolabAMS/resources/views/personal_information/create.blade.php
+++ InnolabAMS/resources/views/personal_information/create.blade.php
@@ -137,7 +137,7 @@
                         </div>
                     </div>
                     <div>
-                        <label class="block text-sm font-medium text-gray-700">Nationality <span class="text-red-500">*</span></label>
+                        <label class="block text-sm font-medium text-gray-700">Nationality <span class="text-red-500">*</span></label> <span class="block text-sm font-medium text-gray-700">Example: Filipino</span>
                         <div class="flex items-center">
                             <input type="text" name="applicant_nationality" required class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
                             <x-form-tooltip text="Enter your nationality as it appears on your passport or government-issued ID" />
