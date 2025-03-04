2fa8fb1e52ccf4c794e296864eb5edec4973dd77
Althea Noelle Sarmiento <absarmiento@student.apc.edu.ph>
2025-03-04 09:42:40 +0800

    feat: Add example text to awards/honors input field

--- InnolabAMS/resources/views/personal_information/create.blade.php
+++ InnolabAMS/resources/views/personal_information/create.blade.php
@@ -292,7 +292,7 @@
                         </div>
                     </div>
                     <div>
-                        <label class="block text-sm font-medium text-gray-700">Awards/Honors</label>
+                        <label class="block text-sm font-medium text-gray-700">Awards/Honors</label> <span class="block text-sm font-medium text-gray-700">Example: Best in Mathematics, Dean's List, etc.</span>
                         <div class="flex items-center">
                             <input type="text" name="awards_honors" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
                         </div>
