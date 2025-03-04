c8cb6335e08a0900ebf6977c08197b165c740c23
Althea Noelle Sarmiento <absarmiento@student.apc.edu.ph>
2025-03-04 13:44:45 +0800

    Added validation translations for Tagalog

---
+++ InnolabAMS/lang/tl/validation.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'accepted' => 'Ang :attribute ay dapat tanggapin.',
+    'accepted_if' => 'Ang :attribute ay dapat tanggapin kapag ang :other ay :value.',
+    'active_url' => 'Ang :attribute ay dapat valid na URL.',
+    // ... add more validation translations ...
+];
