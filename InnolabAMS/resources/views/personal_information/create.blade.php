2c44dabab8f557bb955de05adeaa206f9f326b68
Althea Noelle Sarmiento <absarmiento@student.apc.edu.ph>
2025-03-04 09:32:53 +0800

    refactor: Remove placeholder text from nationality input field

--- InnolabAMS/resources/views/personal_information/create.blade.php
+++ InnolabAMS/resources/views/personal_information/create.blade.php
@@ -142,7 +142,6 @@
                             <input type="text" name="applicant_nationality" required class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
                             <x-form-tooltip text="Enter your nationality as it appears on your passport or government-issued ID" />
                         </div>
-                        <span class="block text-sm font-medium text-gray-700">Example: Filipino</span>
                     </div>
                     <div>
                         <label class="block text-sm font-medium text-gray-700">Religion <span class="text-red-500">*</span></label>
