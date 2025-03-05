d2e2845bd5b5d583b43d497267fd8dd19dd6aad4
Althea Noelle Sarmiento <absarmiento@student.apc.edu.ph>
2025-03-05 19:27:54 +0800

    refactor: Remove telephone number placeholder from input field

--- InnolabAMS/resources/views/personal_information/create.blade.php
+++ InnolabAMS/resources/views/personal_information/create.blade.php
@@ -167,7 +167,6 @@
                         <div class="flex items-center">
                             <input type="tel"
                                    name="applicant_tel_no"
-                                   placeholder="(0X) XXXX-XXXX"
                                    maxlength="15"
                                    oninput="this.value = this.value.slice(0, 15)"
                                    class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
