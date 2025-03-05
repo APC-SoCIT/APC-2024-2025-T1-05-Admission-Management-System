b4a50777a499c3995c5d9f3ad8884b9ff36b4e65
Althea Noelle Sarmiento <absarmiento@student.apc.edu.ph>
2025-03-05 14:04:03 +0800

    feat: Add explanatory text for sex field in personal information form

--- InnolabAMS/resources/views/personal_information/create.blade.php
+++ InnolabAMS/resources/views/personal_information/create.blade.php
@@ -108,7 +108,7 @@
                         </div>
                     </div>
                     <div>
-                        <label class="block text-sm font-medium text-gray-700">Sex <span class="text-red-500">*</span></label>
+                        <label class="block text-sm font-medium text-gray-700">Sex <span class="text-red-500">*</span></label> <span class="block text-sm font-medium text-gray-700">Choose your biological sex</span>
                         <div class="flex items-center">
                             <select name="gender" required class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
                                 <option value="">Select Sex</option>
