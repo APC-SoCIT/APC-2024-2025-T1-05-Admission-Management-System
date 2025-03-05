398c9d4b78424fd2ae3f204f9a6fd031ca7d3a7d
Althea Noelle Sarmiento <absarmiento@student.apc.edu.ph>
2025-03-05 14:00:26 +0800

    remove: Remove tooltip for age field in personal information form

--- InnolabAMS/resources/views/personal_information/create.blade.php
+++ InnolabAMS/resources/views/personal_information/create.blade.php
@@ -128,7 +128,6 @@
                     <div>
                         <label class="block text-sm font-medium text-gray-700">Age <span class="text-red-500">*</span></label>
                         <input type="number" name="age" id="age" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" readonly>
-                        <x-form-tooltip text="Age is automatically calculated based on the date of birth" />
                     </div>
                     <div>
                         <label class="block text-sm font-medium text-gray-700">Place of Birth <span class="text-red-500">*</span></label> <span class="block text-sm font-medium text-gray-700">Example: Manila</span>
