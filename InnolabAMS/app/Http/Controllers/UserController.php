c536da5500632d09a3ce90abdfe8ad81ee3f157b
Reycel John Emmanuel Carcueva <rvcarcueva2@student.apc.edu.ph>
2025-03-05 13:05:11 +0000

    modified user controller

--- InnolabAMS/app/Http/Controllers/UserController.php
+++ InnolabAMS/app/Http/Controllers/UserController.php
@@ -9,6 +9,40 @@ class UserController extends Controller
     public function show()
     {
         $users = User::paginate(10); // Fetch all users from the database
-        return view('user.show', compact('users')); // Pass users to the view
+        $adminCount = User::where('role', 'admin')->count();
+        $staffCount = User::where('role', 'staff')->count();
+        $applicantCount = User::where('role', 'applicant')->count();
+        return view('user.show', compact('users', 'adminCount', 'staffCount', 'applicantCount')); // Pass users to the view
     }
+
+    public function showAdmins()
+    {
+        $adminUsers = User::where('role', 'admin')->paginate(10);
+        $adminCount = $adminUsers->count();
+        $staffCount = User::where('role', 'staff')->count();
+        $applicantCount = User::where('role', 'applicant')->count();
+
+        return view('user.admin', compact('adminUsers', 'adminCount', 'staffCount', 'applicantCount'));
+    }
+
+    public function showStaffs()
+    {
+        $staffUsers = User::where('role', 'staff')->paginate(10);
+        $staffCount = $staffUsers->count();
+        $adminCount = User::where('role', 'admin')->count();
+        $applicantCount = User::where('role', 'applicant')->count();
+
+        return view('user.staff', compact('staffUsers', 'adminCount', 'staffCount', 'applicantCount'));
+    }
+
+    public function showApplicants()
+    {
+        $applicantUsers = User::where('role', 'applicant')->paginate(10);
+        $applicantCount = $applicantUsers->count(); 
+        $adminCount = User::where('role', 'admin')->count();
+        $staffCount = User::where('role', 'staff')->count();
+
+        return view('user.applicant', compact('applicantUsers', 'adminCount', 'staffCount', 'applicantCount'));
+    }
+
 }
