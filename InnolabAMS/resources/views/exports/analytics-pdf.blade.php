dc355afa3e953b77b2c6848623223bc7b1e9940f
Reejay Salinas <rvsalinas@student.apc.edu.ph>
2025-03-08 20:42:46 +0800

    refactor: Update analytics PDF export report title to 'Insights Report'

--- InnolabAMS/resources/views/exports/analytics-pdf.blade.php
+++ InnolabAMS/resources/views/exports/analytics-pdf.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Analytics Report</title>
+    <title>Insights Report</title>
     <style>
         body {
             font-family: Arial, sans-serif;
