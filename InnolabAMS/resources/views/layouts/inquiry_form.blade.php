de474d249938dab9d8a84d8c86750f4dcb09bdae
Reejay Salinas <rvsalinas@student.apc.edu.ph>
2025-03-17 09:22:41 +0800

    style: Change background image in inquiry form layout to enhance visual appeal

--- InnolabAMS/resources/views/layouts/inquiry_form.blade.php
+++ InnolabAMS/resources/views/layouts/inquiry_form.blade.php
@@ -24,7 +24,7 @@
 
     <style>
         body {
-            background-image: url("{{ asset('static/images/school-background-srccmsths.jpg') }}") !important;
+            background-image: url("{{ asset('static/images/COVER4.png') }}") !important;
             background-size: cover !important;
             background-position: center !important;
             background-repeat: no-repeat !important;
